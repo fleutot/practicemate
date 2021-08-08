37d887136646d3b7eb25f3565257d89d77dcd5a3
Gauthier Östervall <gauthier.ostervall@lumenradio.com>
2021-08-08 21:08:57 +0200

    Display current beat

--- metronome.py
+++ metronome.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import QObject, QTimer, pyqtSignal
 
 class Metronome(QObject):
-    tick = pyqtSignal()
+    tick = pyqtSignal(str)
     strong_tick = pyqtSignal()
 
     def __init__(self, parent=None):
@@ -11,18 +11,18 @@ class Metronome(QObject):
         self.timer.timeout.connect(self.tick_cb)
 
         self.strong_period = 4
-        self.current_sub_tick = -1
+        self.current_tick = 0
 
     def start(self):
         self.timer.start(1000)
         self.tick_cb()
 
     def tick_cb(self):
-        self.tick.emit()
+        self.current_tick += 1
+        if self.current_tick > self.strong_period:
+            self.current_tick = 1
 
-        self.current_sub_tick += 1
-        if self.current_sub_tick >= self.strong_period:
-            self.current_sub_tick = 0
+        self.tick.emit(str(self.current_tick))
 
-        if self.current_sub_tick == 0:
+        if self.current_tick == 1:
             self.strong_tick.emit()
