b048c2e979cd5674d58d3bb609e6c5c42eca677f
Gauthier Östervall <gauthier.ostervall@lumenradio.com>
2021-08-09 20:46:14 +0200

    Connect tempo spinbox to metronome

--- metronome.py
+++ metronome.py
@@ -4,7 +4,7 @@ class Metronome(QObject):
     tick = pyqtSignal(str)
     strong_tick = pyqtSignal()
 
-    def __init__(self, strong_period, parent=None):
+    def __init__(self, bpm, strong_period, parent=None):
         super(Metronome, self).__init__(parent)
 
         self.timer = QTimer(self)
@@ -13,8 +13,10 @@ class Metronome(QObject):
         self.strong_period = strong_period
         self.current_tick = 0
 
+        self.tick_period_ms = self.bpm_to_period_ms(bpm)
+
     def start(self):
-        self.timer.start(1000)
+        self.timer.start(self.tick_period_ms)
         self.tick_cb()
 
     def tick_cb(self):
@@ -30,3 +32,10 @@ class Metronome(QObject):
     def set_strong_period(self, period):
         self.strong_period = period
         self.current_tick = 0
+
+    def set_tempo(self, bpm):
+        self.tick_period_ms = self.bpm_to_period_ms(bpm)
+        self.timer.start(self.tick_period_ms)
+
+    def bpm_to_period_ms(self, bpm):
+        return 1000 * 60 / bpm
